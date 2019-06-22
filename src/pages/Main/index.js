0b08522b696136cf7149a46ea98b4b2a5706de2c
MarcosMurillo <murillo.psoft@gmail.com>
2019-06-21 21:45:01 -0300

    Implementando ScrollView com os botões de menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
 
 import { Container } from './styles';
 
@@ -7,6 +9,7 @@ export default function Main() {
   return (
     <Container>
       <Header />
+      <Tabs />
     </Container>
   );
 }
